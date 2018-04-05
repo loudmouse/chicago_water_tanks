7ffaffd3b84df0248775cb68552a39dfce4a8989
Nolan Neeley <nolanneeley@gmail.com>
2018-04-05 10:46:22 -0500

    installed and configured devise for the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :water_tank_locations
+  root 'water_tank_locations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

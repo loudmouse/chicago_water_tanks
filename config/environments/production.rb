7bfbea1a10f3be1d49f7fc0bc8da8fffd5dd8033
Nolan Neeley <nolanneeley@gmail.com>
2018-04-10 23:30:59 -0500

    updated aws credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,10 +93,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
+      bucket: ENV.fetch('chicagowatertanks'),
+      access_key_id: ENV.fetch('AKIAIQWED44XQUNX65WA'),
+      secret_access_key: ENV.fetch('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'),
+      s3_region: ENV.fetch('us-east-1'),
     }
   }
 

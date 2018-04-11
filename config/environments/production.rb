13439c25bcdd3e0aaf14dd3097c98642306d1f52
Nolan Neeley <nolanneeley@gmail.com>
2018-04-10 22:54:55 -0500

    configuring aws s3 again for image hosting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # config/environments/production.rb
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('chicagowatertanks'),
+      access_key_id: ENV.fetch('AKIAIQWED44XQUNX65WA'),
+      secret_access_key: ENV.fetch('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'),
+      s3_region: ENV.fetch('us-east-1'),
+    }
+  }
 
 end
